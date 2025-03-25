130936e7ea75c2d10ee7afafb9f5eb7cbd88b9db
Aniket Anand <41674276+anandify@users.noreply.github.com>
2025-03-26 02:19:54 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'https://biltygen-backend.onrender.com/';
+const API_URL = 'https://biltygen-backend.onrender.com';
 
 const api = {
   // Bilty endpoints
